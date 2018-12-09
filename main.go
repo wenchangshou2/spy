1b89ff0152bba4389d001d0302b29ba15d44dae0
wcs <wcs@wcss-iMac.lan>
2018-12-09 23:53:20 +0800

    update

--- main.go
+++ main.go
@@ -1,5 +1,54 @@
 package main
 
+import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"os"
+)
+type config struct{
+	File struct{
+		Path string `yaml:"path"`
+	}
+	List[] string `yaml:"ignore"`
+}
+type downloadContent struct{
+	MainVersion string
+	SubVersion string
+	DownloadUrl string
+	Md5 string
+}
+var file_save_path string
+var ignoreList  []string
+var downloadUrlList []downloadContent
+func init() {
+	c:=new(config)
+	yamlFile, err := ioutil.ReadFile("config.yaml")
+
+	//log.Println("yamlFile:", string(yamlFile))
+	if err != nil {
+		log.Printf("yamlFile.Get err #%v ", err)
+	}
+	err = yaml.Unmarshal(yamlFile, c)
+	if err!=nil{
+		log.Fatal("解析失败")
+	}
+	file_save_path=c.File.Path
+	ignoreList=c.List
+	log.Println(ignoreList)
+
+}
+func createDirectory()  {//创建目录
+	if _,err:=os.Stat(file_save_path);os.IsNotExist(err){
+		os.MkdirAll(file_save_path,os.ModePerm)
+	}
+}
 func main() {
+	createDirectory()
 	spy("https://download.qt.io/archive/qt/")
+	fmt.Println(len(downloadUrlList))
+	for k,v:=range downloadUrlList{
+		fmt.Println(k,v)
+	}
 }
--- spy.go
+++ spy.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"regexp"
 	"strings"
+	//"time"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/wenchangshou2/Spy/core"
@@ -54,7 +56,23 @@ func getNextURL(url string) {
 		fmt.Println(updateTime, version)
 	})
 }
+func getDownloadFileMd5(url string,name string)(string,error){
+	c,err:=getHTMLContext(url+"md5sums.txt")
+	if err!=nil{
+		return "",errors.New("下载失败")
+	}
+	scanner:=bufio.NewScanner(strings.NewReader(c))
+	for scanner.Scan(){
+		txt:=scanner.Text()
+		if strings.Index(txt,name)>-1{
+			idx:=strings.Index(txt," ")
+			fmt.Println(txt,txt[0:idx])
+			return txt[0:idx],nil
+		}
+	}
+	return "",errors.New("没有找到对应的内容")
 
+}
 // 获取下载的链接
 func getDownURL(url string) {
 	resStr, err := getHTMLContext(url)
@@ -71,15 +89,41 @@ func getDownURL(url string) {
 			return
 		}
 		downloadText := selection.Find("td:nth-child(2)").Text()
-		updateTime := selection.Find("td:nth-child(3)").Text()
-		fmt.Println(updateTime)
-		if m,_:=regexp.MatchString(downloadText,"windows");!m{
+		//updateTime := selection.Find("td:nth-child(3)").Text()
 
+
+		if i:=strings.Index(downloadText,"exe");i>-1{
+			urlList:=strings.Split(url,"/")
+			m,_:=getDownloadFileMd5(url,downloadText)
+			d:=downloadContent{}
+			d.MainVersion=urlList[len(urlList)-3]
+			d.SubVersion=urlList[len(urlList)-2]
+			d.Md5=m
+			d.DownloadUrl=url+downloadText
+			downloadUrlList=append(downloadUrlList, d)
 		}
 	})
 }
+
+func isIgore(url string)bool{
+	list:=strings.Split(url,"/")
+	version:=list[len(list)-2]
+	if m,_:=regexp.MatchString("\\d+",version);m{
+		for _,v:=range ignoreList{
+			if strings.Compare(v,version) ==0{
+				return true
+			}
+
+		}
+	}
+	return false
+}
 func spy(url string) {
 	fmt.Println(url)
+	if isIgore(url){
+		return
+	}
+	//return
 	if len(strings.Split(url, "/")) < 8 {
 		getNextURL(url)
 	} else {
